9d6bb1e2d55040f7f62a5668f58bec9f6360ecdd
Martin Schalter <schalter.dev@gmail.com>
2019-04-16 13:21:17 +0200

    add profile screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quotierte_redeliste/create_room_screen.dart';
+import 'package:quotierte_redeliste/profile_screen.dart';
 
 class StartScreen extends StatelessWidget {
   StartScreen({Key key}) : super(key: key);
@@ -32,52 +33,80 @@ class StartScreen extends StatelessWidget {
             icon: Icon(Icons.account_circle),
             tooltip: 'Profil bearbeiten',
             onPressed: () {
-              // TODO ...
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfileScreen()),
+              );
             },
           ),
         ],
       ),
       // body is the majority of the screen.
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text('Code zum Beitreten eingeben'),
-          Row(children: [
-            TextField(onSubmitted: (code) {
-              // TODO ...
-            }),
-            RaisedButton(
-                onPressed: () {
-                  // TODO ...
-                },
-                child: const Text('beitreten'))
-          ]),
-          Text('Oder'),
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CreateRoomScreen()),
-              );
-            },
-            child: const Text('+ neuen Raum erstellen'),
+          Padding(
+            padding: EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 30),
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Raum beitreten',
+                      style: Theme.of(context).textTheme.title),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 5),
+                  ),
+                  Row(children: [
+                    Flexible(
+                        child: TextField(
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(3),
+                                labelText: 'Code zum Beitreten'),
+                            onSubmitted: (code) {
+                              // TODO ...
+                            })),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    RaisedButton(
+                        onPressed: () {
+                          // TODO ...
+                        },
+                        child: const Text('beitreten'))
+                  ]),
+                  Padding(
+                    padding: EdgeInsets.only(top: 25, bottom: 5),
+                    child: Text('Raum erstellen',
+                        style: Theme.of(context).textTheme.title),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CreateRoomScreen()),
+                      );
+                    },
+                    child: const Text('+ neuen Raum erstellen'),
+                  ),
+                ]),
           ),
+          Text('Historie', style: Theme.of(context).textTheme.title),
+          Padding(padding: EdgeInsets.only(bottom: 8)),
           Expanded(
               child: ListView.builder(
             itemCount: rooms.length,
             itemBuilder: (context, pos) {
               return Padding(
-                  padding: EdgeInsets.only(bottom: 4.0),
+                  padding: EdgeInsets.only(bottom: 2.0, left: 4, right: 4),
                   child: Card(
-                    color: Colors.white,
                     child: Padding(
                       padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
                       child: Text(
                         rooms[pos],
                         style: TextStyle(
                           fontSize: 18.0,
-                          height: 1.6,
                         ),
                       ),
                     ),
