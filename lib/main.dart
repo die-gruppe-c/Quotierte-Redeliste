755eae9ab05fb7b43d79299e3c77d3f17cf8c6a4
Martin Schalter <schalter.dev@gmail.com>
2019-05-07 09:12:49 +0200

    fix compile errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 // This sample shows adding an action to an [AppBar] that opens a shopping cart.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quotierte_redeliste/ui/moderator_screen/moderator_screen.dart';
 import 'package:quotierte_redeliste/ui/start_screen/start_screen.dart';
-
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.white, //top bar color
-    statusBarIconBrightness: Brightness.dark, //top bar icons
-    systemNavigationBarColor: Colors.white, //bottom bar color
-    systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
-  ));
-  runApp(MyApp());
-}
 
 const PRIMARY_COLOR = Colors.white;
 const PRIMARY_COLOR_DARK = Colors.black87;
 const HINT_COLOR = Colors.black38;
 const ACCENT_COLOR = Colors.red;
 const DISABLED_COLOR = Colors.black26;
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: PRIMARY_COLOR, //top bar color
+    statusBarIconBrightness: Brightness.dark, //top bar icons
+    systemNavigationBarColor: PRIMARY_COLOR, //bottom bar color
+    systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
+  ));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
