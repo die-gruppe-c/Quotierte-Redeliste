f1080fb0cd64f730689da9d7972d3b817b1e59ba
Chris <roth.christopher8@gmail.com>
2019-05-21 15:26:20 +0200

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:quotierte_redeliste/ui/moderator_screen/moderator_screen.dart';
-import 'package:quotierte_redeliste/ui/start_screen/start_screen.dart';
+import 'package:quotierte_redeliste/ui/splash_screen/splash_screen.dart';
 
 const PRIMARY_COLOR = Colors.white;
 const PRIMARY_COLOR_DARK = Colors.black87;
@@ -60,7 +60,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Quoty',
             theme: theme,
-            home: StartScreen(),
+            home: SplashScreen(),
             routes: {
               "/room/moderator": (_) => new ModeratorScreen(),
             },
---
+++ lib/ui/splash_screen/splash_screen.dart
@@ -0,0 +1,87 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quotierte_redeliste/models/room.dart';
+import 'package:quotierte_redeliste/resources/repository.dart';
+import 'package:quotierte_redeliste/ui/start_screen/start_screen.dart';
+import 'package:quotierte_redeliste/ui/waiting_room/waiting_room_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/images/icon_placeholder.png',),
+              Container(
+                padding: const EdgeInsets.only(top: 32),
+                child: CircularProgressIndicator(),
+              )
+            ]
+        )
+      )
+    );
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _connectToServer();
+  }
+
+  _connectToServer(){
+    Repository().getRoomToJoin().then((Room room){
+      var screen;
+
+      if(room == null) screen = StartScreen();
+      else screen = WaitingRoomScreen();
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => screen),
+      );
+    }).catchError((e) {
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Verbindung zum Server konnte nicht hergestellt werden...'),
+            actions: [
+              FlatButton(
+                child: Text('SCHLIESSEN'),
+                onPressed: () {
+                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                },
+              ),
+              FlatButton(
+                child: Text('ERNEUT VERSUCHEN'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _connectToServer();
+                },
+              )
+            ],
+          );
+        },
+      );
+    });
+  }
+}
